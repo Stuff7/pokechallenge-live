335fd077f8255b038e228e19e6c67ceecdb99d3d
Armando M <47931658+Stuff7@users.noreply.github.com>
2020-03-06 00:08:13 -0600

    Using react-scripts build

---
+++ precache-manifest.1b10aa0bbd69f4adddfbcb0b8dba305f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb5425688633477b9c525cd90c0d0ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46bcacba7b8f4442b641",
+    "url": "./static/js/2.7b6a3f63.chunk.js"
+  },
+  {
+    "revision": "23267f18d58dbf607918863f130a5217",
+    "url": "./static/js/2.7b6a3f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b3c5026a1b28f68698",
+    "url": "./static/js/main.2a174a42.chunk.js"
+  },
+  {
+    "revision": "b9548a7a78f1374ca985",
+    "url": "./static/js/runtime-main.8fac7684.js"
+  }
+]);
